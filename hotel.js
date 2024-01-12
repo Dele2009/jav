bd8ca5ac54cb5a6bd961818b3b86e7399c34b7db
Dele2009 <obedaminu303@gmail.com>
2024-01-12 12:30:38 +0100

    hotel app feature completed

--- hotel.js
+++ hotel.js
@@ -1,35 +1,39 @@
-let fname = document.getElementById("form-fname");
-let lname = document.getElementById("form-lname");
-let fmail = document.getElementById("form-email");
-let fconta = document.getElementById("form-cont");
-let fsubmit = document.getElementById("form-info-sub");
-let emailformat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-let contactformat = /^(?:(\+234)|(0))([789][01])(\d{8})$/;
-let formpage = document.getElementById("form1");
-let formpagetwo = document.getElementById("form2");
-let loader = document.getElementById("loadman");
+const fname = document.getElementById("form-fname");
+const lname = document.getElementById("form-lname");
+const fmail = document.getElementById("form-email");
+const fconta = document.getElementById("form-cont");
+const fsubmit = document.getElementById("form-info-sub");
+const emailformat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const contactformat = /^(?:(\+234)|(0))([789][01])(\d{8})$/;
+const formpage = document.getElementById("form1");
+const formpagetwo = document.getElementById("form2");
+const loader = document.getElementById("loadman");
 
 
-let depart = document.getElementById("datedepart");
-let rooms = document.getElementById("Rooms");
-let roomprice=document.getElementById("price-room-div");
-let datediv=document.getElementById("dates-ar");
-let duluxprices=document.getElementById("price-room");
-let priceoutput = document.getElementById("priceout");
-let pricediv = document.getElementById("price-sys");
-let hotelnext = document.getElementById("hotelnext");
-let hotelbook = document.getElementById("hotelbook");
-let countdisplay=document.getElementById("countdown");
-let section=document.getElementById("section-mid");
-let formaway=document.querySelector(".forms");
-let cardtext=document.querySelector(".card-text")
-let cardcont=document.querySelector(".card-container")
+const depart = document.getElementById("datedepart");
+const rooms = document.getElementById("Rooms");
+const roomprice = document.getElementById("price-room-div");
+const datediv = document.getElementById("dates-ar");
+const duluxprices = document.getElementById("price-room");
+const priceoutput = document.getElementById("priceout");
+const pricediv = document.getElementById("price-sys");
+const hotelnext = document.getElementById("hotelnext");
+const hotelbook = document.getElementById("hotelbook");
+const countdisplay = document.getElementById("countdown");
+const section = document.getElementById("section-mid");
+const formaway = document.querySelector(".forms");
+const cardtext = document.querySelector(".card-text")
+const cardcont = document.querySelector(".card-container")
 
 
 
 
-let proceed, daydis, day, hour, min, sec,initialstay,finalstay,counters;
+let proceed, daydis, day, hour, min, sec, initialstay, finalstay, counters;
 
+
+
+
+//INNITIAL FORM VALIDATIONS
 function formname() {
     if (!fname.value || fname.value.length > 15) {
         proceed = false;
@@ -77,6 +81,46 @@ function formcontact() {
 
 
 
+
+
+//FUNCTION FOR LOADER DURATION
+function loadershow(loading, stop) {
+    let count = loading;
+
+    function loaderinin() {
+        console.log(count);
+        if (count === stop) {
+            clearInterval(goload)
+            loader.style.visibility = "hidden";
+            loader.style.position = "absolute";
+            formpagetwo.style.position = "relative";
+            formpagetwo.style.visibility = "visible";
+            formpage.style.position = "absolute";
+        }
+        else {
+
+            count++
+
+            loader.style.visibility = "visible";
+            loader.style.position = "relative";
+        }
+    }
+    let goload = setInterval(loaderinin, 1000)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //LOADER AND NEXT FORM INNITIATOR
 fsubmit.addEventListener("click", () => {
 
@@ -92,210 +136,244 @@ fsubmit.addEventListener("click", () => {
         formpage.style.visibility = "hidden";
         formpage.style.position = "relative";
 
-        //FUNCTION FOR LOADER DURATION
-        function loadershow(loading, stop) {
-            // let count = loading;
 
-            function loaderinin() {
-                console.log(loading);
-                if (loading == stop) {
-                    clearInterval(goload)
-                    loader.style.visibility = "hidden";
-                    loader.style.position = "absolute";
-                    formpagetwo.style.position = "relative";
-                    formpagetwo.style.visibility = "visible";
-                    formpage.style.position = "absolute";
-                }
-                else {
 
-                    loading++
-
-                    loader.style.visibility = "visible";
-                    loader.style.position = "relative";
-                }
-            }
-            let goload = setInterval(loaderinin, 1000)
-
-        }
-        loadershow(0, 3)
+        loadershow(0, 3);
 
     }
 
 
 
 })
-function loadsel(){
-    if(rooms.value==="0"){
-        roomprice.style.visibility="hidden";
-        datediv.style.visibility="hidden";
-        datediv.style.position="absolute";
 
-
-        hotelnext.style.visibility="visible";
-        hotelnext.style.position="relative";
-        hotelbook.style.visibility="hidden";
-        hotelbook.style.position="absolute";
-        pricediv.style.visibility="hidden";
-        pricediv.style.position="absolute";
-
-        
-        depart.value="";
-    }
-    if(rooms.value==="2"){
-        roomprice.style.visibility="visible";
-        roomprice.style.position="relative";
-
-        datediv.style.visibility="hidden";
-        datediv.style.position="absolute";
-
-
-        hotelnext.style.visibility="visible";
-        hotelnext.style.position="relative";
-        hotelbook.style.visibility="hidden";
-        hotelbook.style.position="absolute";
-        pricediv.style.visibility="hidden";
-        pricediv.style.position="absolute";
-        
-        depart.value="";
-        
-
-    }
-    else{
-        datediv.style.visibility="visible";
-        datediv.style.position="relative";
-        roomprice.style.visibility="hidden";
-
-        // hotelbook.style.width="80%";
-        // hotelnext.style.width="80%";
-
-
-        hotelnext.style.visibility="visible";
-        hotelnext.style.position="relative";
-        hotelbook.style.visibility="hidden";
-        hotelbook.style.position="absolute";
-        pricediv.style.visibility="hidden";
-        pricediv.style.position="absolute";
-
-        
-        depart.value="";
-    }
-
-}
-function loaddate(){
-    if(duluxprices.value!="0"){
-        datediv.style.visibility="visible";
-        datediv.style.position="relative";
-        // hotelbook.style.width="80%";
-        // hotelnext.style.width="80%";
-
-        hotelnext.style.visibility="visible";
-        hotelnext.style.position="relative";
-        hotelbook.style.visibility="hidden";
-        hotelbook.style.position="absolute";
-        pricediv.style.visibility="hidden";
-        pricediv.style.position="absolute";
-        
-        
-
-    }
-    else{
-        datediv.style.visibility="visible";
-        datediv.style.position="relative";
-
-        hotelnext.style.visibility="visible";
-        hotelnext.style.position="relative";
-        hotelbook.style.visibility="hidden";
-        hotelbook.style.position="absolute";
-        pricediv.style.visibility="hidden";
-        pricediv.style.position="absolute";
-       
-    }
+function datechange() {
+    pricediv.style.visibility = "hidden";
+    pricediv.style.position = "absolute";
+    hotelnext.style.visibility = "visible";
+    hotelnext.style.position = "relative";
+    hotelbook.style.visibility = "hidden";
+    hotelbook.style.position = "absolute";
+    priceoutput.innerHTML = "";
 }
 
-function datechange(){
-    pricediv.style.visibility="hidden";
-    pricediv.style.position="absolute";
-    hotelnext.style.visibility="visible";
-    hotelnext.style.position="relative";
-    hotelbook.style.visibility="hidden";
-    hotelbook.style.position="absolute";
-    priceoutput.innerHTML="";
-}
 
- function loadform()  {
-   
-    
 
-    
 
-    
 
-    if (!depart.value || rooms.value === "0") {
-        arrival.style.border = "1px solid red";
-        depart.style.border = "1px solid red";
-        rooms.style.border = "1px solid red";
-        pricediv.style.visibility="visible";
-        pricediv.style.position="relative";
-        priceoutput.innerHTML="Error pls check inputs";
+
+function loadsel() {
+
+
+    depart.style.border = "";
+    depart.value = "";
+    rooms.style.border = "";
+    if (rooms.value === "0") {
+        roomprice.style.visibility = "hidden";
+        datediv.style.visibility = "hidden";
+        datediv.style.position = "absolute";
+
+
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "hidden";
+        pricediv.style.position = "absolute";
+
+
+        depart.value = "";
+    }
+    if (rooms.value === "2") {
+        roomprice.style.visibility = "visible";
+        roomprice.style.position = "relative";
+
+        datediv.style.visibility = "hidden";
+        datediv.style.position = "absolute";
+
+
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "hidden";
+        pricediv.style.position = "absolute";
+
+        depart.value = "";
+
 
     }
-   
-
-
-
-   
     else {
-       
+        datediv.style.visibility = "visible";
+        datediv.style.position = "relative";
+        roomprice.style.visibility = "hidden";
 
-        initialstay = +(new Date().getTime());
-        console.log(initialstay);
-        finalstay = +(new Date(`${depart.value}`).getTime());
-        console.log(finalstay);
 
-        daydis = finalstay - initialstay;
-        day = Math.floor(daydis / (1000 * 60 * 60 * 24));
-        console.log(day);
 
-        if(rooms.value=="5000"){
-            priceoutput.innerHTML=`You have now chosen the regular residential suite since you'll be staying with us for ${day}days your total bill is ${rooms.value*day}`;
-            hotelnext.style.visibility="hidden";
-            hotelnext.style.position="absolute";
-            hotelbook.style.visibility="visible";
-            hotelbook.style.position="relative";
-            
-            pricediv.style.visibility="visible";
-            pricediv.style.position="relative";
-            datediv.style.visibility="visible";
-            datediv.style.position="relative";
-            
 
-        }
-        if(rooms.value=="2"){
-            priceoutput.innerHTML=`You have now chosen the Duluxe residential suite, since you'll be staying with us for ${day}days your total bill is ${duluxprices.value*day}`
-            hotelnext.style.visibility="hidden";
-            hotelnext.style.position="absolute";
-            hotelbook.style.visibility="visible";
-            hotelbook.style.position="relative";
-            pricediv.style.visibility="visible";
-            pricediv.style.position="relative";
-            datediv.style.visibility="visible";
-            datediv.style.position="relative";
-            // hotelbook.style.width="100%";
-                                    
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "hidden";
+        pricediv.style.position = "absolute";
 
-        }
 
-       
+        depart.value = "";
     }
-   
 
- }
+}
 
- 
- hotelnext.addEventListener("click",loadform)
 
-hotelbook.addEventListener("click",()=>{
-    function starcount(){
+
+
+function loaddate() {
+    depart.style.border = "";
+    depart.value = "";
+    rooms.style.border = "";
+    if (duluxprices.value != "0") {
+        datediv.style.visibility = "visible";
+        datediv.style.position = "relative";
+
+
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "hidden";
+        pricediv.style.position = "absolute";
+
+
+
+    }
+    else {
+        datediv.style.visibility = "visible";
+        datediv.style.position = "relative";
+
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "hidden";
+        pricediv.style.position = "absolute";
+
+    }
+}
+
+
+
+
+
+
+
+
+function loadform() {
+
+    initialstay = +(new Date().getTime());
+    console.log(initialstay);
+    finalstay = +(new Date(`${depart.value}`).getTime());
+    console.log(finalstay);
+
+    daydis = finalstay - initialstay;
+    day = Math.floor(daydis / (1000 * 60 * 60 * 24));
+    console.log(day);
+
+    if (!depart.value) {
+        depart.style.border = "1px solid red";
+
+        pricediv.style.visibility = "visible";
+        pricediv.style.position = "relative";
+        priceoutput.innerHTML = "Error pls check inputs";
+
+    }
+    else if (rooms.value === "0") {
+        rooms.style.border = "1px solid red";
+        pricediv.style.visibility = "visible";
+        pricediv.style.position = "relative";
+        priceoutput.innerHTML = "Room type selection invalid";
+    }
+    else if (day < 0 && day < -1) {
+        priceoutput.innerHTML = "Date value invalid";
+        depart.style.border = "1px solid red";
+        hotelnext.style.visibility = "visible";
+        hotelnext.style.position = "relative";
+        hotelbook.style.visibility = "hidden";
+        hotelbook.style.position = "absolute";
+        pricediv.style.visibility = "visible";
+        pricediv.style.position = "relative";
+
+    }
+    else if (day <= 0 && rooms.value == "5000") {
+        priceoutput.innerHTML = `You have now chosen the regular residential suite since you'll be staying with us for a night your total bill is ${rooms.value}`;
+        hotelnext.style.visibility = "hidden";
+        hotelnext.style.position = "absolute";
+        hotelbook.style.visibility = "visible";
+        hotelbook.style.position = "relative";
+
+        pricediv.style.visibility = "visible";
+        pricediv.style.position = "relative";
+        datediv.style.visibility = "visible";
+        datediv.style.position = "relative";
+    }
+    else if (day <= 0 && rooms.value == "2") {
+        priceoutput.innerHTML = `You have now chosen the Duluxe residential suite since you'll be staying with us for a night your total bill is ${duluxprices.value}`;
+        hotelnext.style.visibility = "hidden";
+        hotelnext.style.position = "absolute";
+        hotelbook.style.visibility = "visible";
+        hotelbook.style.position = "relative";
+
+        pricediv.style.visibility = "visible";
+        pricediv.style.position = "relative";
+        datediv.style.visibility = "visible";
+        datediv.style.position = "relative";
+    }
+
+    else {
+        depart.style.border = "";
+        rooms.style.border = "";
+
+
+
+
+        if (rooms.value === "5000") {
+            priceoutput.innerHTML = `You have now chosen the regular residential suite since you'll be staying with us for ${day}days your total bill is ${rooms.value * day}`;
+            hotelnext.style.visibility = "hidden";
+            hotelnext.style.position = "absolute";
+            hotelbook.style.visibility = "visible";
+            hotelbook.style.position = "relative";
+
+            pricediv.style.visibility = "visible";
+            pricediv.style.position = "relative";
+            datediv.style.visibility = "visible";
+            datediv.style.position = "relative";
+
+
+        }
+        if (rooms.value === "2") {
+            priceoutput.innerHTML = `You have now chosen the Duluxe residential suite, since you'll be staying with us for ${day}days your total bill is ${duluxprices.value * day}`
+            hotelnext.style.visibility = "hidden";
+            hotelnext.style.position = "absolute";
+            hotelbook.style.visibility = "visible";
+            hotelbook.style.position = "relative";
+            pricediv.style.visibility = "visible";
+            pricediv.style.position = "relative";
+            datediv.style.visibility = "visible";
+            datediv.style.position = "relative";
+
+
+
+        }
+
+
+    }
+
+
+}
+
+
+hotelnext.addEventListener("click", loadform)
+
+hotelbook.addEventListener("click", () => {
+    function starcount() {
         initialstay = +(new Date().getTime());
         console.log(initialstay);
         finalstay = +(new Date(`${depart.value}`).getTime());
@@ -303,35 +381,32 @@ hotelbook.addEventListener("click",()=>{
 
 
         daydis = finalstay - initialstay;
-        day = Math.floor(daydis / (1000 * 60 * 60 * 24)).toString().padStart(2,"0");
-        
-        hour=Math.floor((daydis%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)).toString().padStart(2,"0");
-        min=Math.floor((daydis%(1000 * 60 * 60 ))/(1000 * 60)).toString().padStart(2,"0");
-        sec=Math.floor((daydis%(1000 * 60))/(1000)).toString().padStart(2,"0");
-        // console.log(day);
-        // console.log(hour);
-        // console.log(min);
-        // console.log(sec);
-        countdisplay.innerHTML=` ${day}<span style="color: #460404;">days</span> : 
+        day = Math.floor(daydis / (1000 * 60 * 60 * 24)).toString().padStart(2, "0");
+
+        hour = Math.floor((daydis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, "0");
+        min = Math.floor((daydis % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0");
+        sec = Math.floor((daydis % (1000 * 60)) / (1000)).toString().padStart(2, "0");
+
+        countdisplay.innerHTML = ` ${day}<span style="color: #460404;">days</span> : 
                                  ${hour}<span style="color: #460404;">hrs</span> : 
                                  ${min}<span style="color: #460404;">m</span> : 
                                  ${sec}<span style="color: #460404;">s</span>`
-        
-        cardtext.innerHTML=`Dear ${fname.value} ${lname.value} we appreciate your patrontage,
+
+        cardtext.innerHTML = `Dear ${fname.value} ${lname.value} we appreciate your patrontage,
                             here at Home-suites we value customer satisfaction, please kindly
                             make your way to the reception if you are in need of futher assistance `;
 
-        section.style.visibility="hidden";
-        section.style.position="absolute"; 
-        cardcont.style.position="relative";
-        cardcont.style.visibility="visible";
-          
-        formaway.style.display="none";
-        
-                            
-    
+        section.style.visibility = "hidden";
+        section.style.position = "absolute";
+        cardcont.style.position = "relative";
+        cardcont.style.visibility = "visible";
+
+        formaway.style.display = "none";
+
+
+
     }
-    counters=setInterval(starcount,1000)
+    counters = setInterval(starcount, 1000)
 
 })
 
